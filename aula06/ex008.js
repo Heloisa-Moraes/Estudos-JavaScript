530454e4e11fe2e957054131a88afb147020360b
Heloisa-Moraes <helomoraes2009@hotmail.com>
2024-09-11 10:40:22 -0300

    Aula 1 condições (if e else); Desafio da nacionalidade

---
+++ aula06/ex008.js
@@ -0,0 +1,8 @@
+var vel = 78.9 
+
+console.log(`A velocidade do carro é ${vel} km/h`)
+
+if (vel >60) { //condição simples 
+    console.log("Você ultrapassou o limite, está multado!")
+}
+console.log("Dirija sempre usando o cinto de segurança!")
